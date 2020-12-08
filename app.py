7cc1b56cb88c61c6555091ab61a70fb6aaf3eb38
Noitral <2016200018@ruc.edu.cn>
2020-12-08 11:14:08 +0800

    I'm ready!

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Grey Li'
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
